018e3c9c4af2a7276b99eb61af66d3d3375c090c
youngsfamilyphone <30782967+youngsfamilyphone@users.noreply.github.com>
2020-05-05 15:24:18 -0600

    Update simple-event-logger.groovy

--- smartapps/krlaframboise/simple-event-logger.src/simple-event-logger.groovy
+++ smartapps/krlaframboise/simple-event-logger.src/simple-event-logger.groovy
@@ -708,7 +708,7 @@ private getRoundInterval() {
 
 // Google Sheets redirects the post to a temporary url so the response is usually 302 which is page moved.
 def processLogEventsResponse(response, data) {
-	logTrace "Sheets response: " + response?.getData();
+	//logTrace "Sheets response: " + response?.getData();
 	logTrace "error: " + response?.getErrorData();
 	logTrace " data: " + data;
 	if (response?.status == 302) {
